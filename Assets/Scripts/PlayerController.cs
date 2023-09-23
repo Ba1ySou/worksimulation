619bd196e8aaeae44f207355887c55afa420500e
Ba1ySou <Ba1ySou@163.com>
2023-09-23 19:21:36 +0800

    Update PlayerController.cs
    优化移动逻辑（新增WASD移动）

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,27 +10,33 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (UnityEngine.Input.GetKeyDown(KeyCode.RightArrow) || UnityEngine.Input.GetKeyDown(KeyCode.D))   
             moveDir = Vector2.right;
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (UnityEngine.Input.GetKeyDown(KeyCode.LeftArrow) || UnityEngine.Input.GetKeyDown(KeyCode.A))
             moveDir = Vector2.left;
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (UnityEngine.Input.GetKeyDown(KeyCode.UpArrow) || UnityEngine.Input.GetKeyDown(KeyCode.W))
             moveDir = Vector2.up;
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (UnityEngine.Input.GetKeyDown(KeyCode.DownArrow) || UnityEngine.Input.GetKeyDown(KeyCode.S))
             moveDir = Vector2.down;
 
-        if(moveDir != Vector2.zero)
+        if (moveDir != Vector2.zero)
         {
-            if(CanMoveToDir(moveDir))
+            if (CanMoveToDir(moveDir))
             {
                 Move(moveDir);
             }
         }
 
         moveDir = Vector2.zero;
+       
+        
+        
+        
+        
+        //NewMove();    //使用InputManager控制角色，但我放弃了，没办法解决按下时是顺滑移动的问题，如果感兴趣的话可以把注释消掉玩一玩（把上面那些移动代码记得注释掉）
     }
 
     bool CanMoveToDir(Vector2 dir)
@@ -51,4 +58,17 @@ public class PlayerController : MonoBehaviour
     {
         transform.Translate(dir);
     }
+
+    void NewMove()  //使用Unity的InputManager来控制角色
+    {
+        float horizontal = UnityEngine.Input.GetAxisRaw("Horizontal"); //获取水平轴移动
+        float vertical = UnityEngine.Input.GetAxisRaw("Vertical");  //获取垂直轴移动
+
+        Vector2 dir = new Vector3(horizontal, vertical, 0);//根据输入得到移动方向
+        transform.Translate(dir * Time.deltaTime ); //修改位置
+
+
+        //transform.Translate(dir)
+    }
+
 }
